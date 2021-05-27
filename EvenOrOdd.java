b002cd03ab9c023c4ed681812ce318e368e06124
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-27 20:47:58 +0530

    ADDED:EvenOrOdd code

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		int num = 9;
+		if (num % 2 == 0) {
+
+			System.out.println("Entered Number is Even");
+		} else {
+			System.out.println("Entered Number is Odd");
+		}
+	}
+}
