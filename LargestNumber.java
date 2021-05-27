2945135eddbdeda823c48bb598d3fcd94389ac84
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-27 20:55:55 +0530

    ADDED:LargestNumber code

---
+++ LargestNumber.java
@@ -0,0 +1,16 @@
+public class LargestNumber {
+	    public static void main(String[] args) {
+
+	        double n1 = 25, n2 = 60, n3 = 9;
+
+	        if( n1 >= n2 && n1 >= n3)
+	            System.out.println(n1 + " is the largest number.");
+
+	        else if (n2 >= n1 && n2 >= n3)
+	            System.out.println(n2 + " is the largest number.");
+
+	        else
+	            System.out.println(n3 + " is the largest number.");
+	    }
+	}
+
